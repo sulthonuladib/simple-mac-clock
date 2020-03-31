21abb7998c2742fa75c3988244df45113a35a419
sulthonuladib <adibcyber4rt@gmail.com>
2020-03-31 17:46:00 +0700

    Initial Commit

---
+++ Simple Mac Clock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simple Mac Clock
+//
+//  Created by Sulthonul Adib on 31/03/20.
+//  Copyright © 2020 Sulthonul Adib. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
